adc3e6e770119fa1b4ff7fa97559a215ed16370f
IL-HH <ismailhamdach@gmail.com>
2024-11-08 13:58:07 +0300

    add title to the app

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -14,8 +14,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Qallam Educational Platform",
+  description: "This is Home for Qallam Educational Platform",
   // other metadata
 };
 
